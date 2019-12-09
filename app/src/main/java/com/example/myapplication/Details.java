2ecf947c9aa5a3c6faab6bba2e4570feafacc77e
sruthikilari <skilari2@illinois.edu>
2019-12-08 22:23:48 -0600

    details class updated

--- app/src/main/java/com/example/myapplication/Details.java
+++ app/src/main/java/com/example/myapplication/Details.java
@@ -1,6 +1,32 @@
 package com.example.myapplication;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Details extends AppCompatActivity {
+
+    private Button searchAgain;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.details);
+        searchAgain = (Button) findViewById(R.id.searchAgain);
+        searchAgain.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                launchMain();
+            }
+        });
+
+    }
+
+    private void launchMain() {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
 }
--- app/src/main/java/com/example/myapplication/NewSearch.java
+++ app/src/main/java/com/example/myapplication/NewSearch.java
@@ -4,10 +4,10 @@ package com.example.myapplication;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.AdapterView;
+//import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.ScrollView;
+//import android.widget.ScrollView;
 import android.widget.Spinner;
 //import android.widget.Button;
 
