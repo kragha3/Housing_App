5fc51db85a895fbd01ead645605f5b9f07643d5c
sruthikilari <skilari2@illinois.edu>
2019-12-08 20:40:53 -0600

    apartment names upadted

--- app/src/main/java/com/example/myapplication/Apartment.java
+++ app/src/main/java/com/example/myapplication/Apartment.java
@@ -1,16 +1,22 @@
 package com.example.myapplication;
 
 public class Apartment {
+    private String name;
     private int price;
     private int bedrooms;
     private int bathrooms;
     private String location;
     private int points;
-    Apartment(int setPrice, int setRooms, int setBathrooms, String setLocation) {
+    Apartment(String setName, int setPrice, int setRooms, int setBathrooms, String setLocation) {
+        name = setName;
         price = setPrice;
         bedrooms = setRooms;
         bathrooms = setBathrooms;
         location = setLocation;
+    }
+
+    public String getName() {
+        return name;
     }
     public int getPrice() {
         return price;
