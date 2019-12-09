7fbc9249c6416f2b87a3398a0e62148b16b4f3e7
sruthikilari <skilari2@illinois.edu>
2019-12-08 20:51:15 -0600

    price and location indexes added

--- app/src/main/java/com/example/myapplication/Apartment.java
+++ app/src/main/java/com/example/myapplication/Apartment.java
@@ -3,13 +3,17 @@ package com.example.myapplication;
 public class Apartment {
     private String name;
     private int price;
+    private int priceIndex;
     private int bedrooms;
     private int bathrooms;
     private String location;
+    private int locationIndex;
     private int points;
-    Apartment(String setName, int setPrice, int setRooms, int setBathrooms, String setLocation) {
+    Apartment(String setName, int setPrice, int setPriceIndex, int setLocationIndex, int setRooms, int setBathrooms, String setLocation) {
         name = setName;
         price = setPrice;
+        priceIndex = setPriceIndex;
+        locationIndex = setLocationIndex;
         bedrooms = setRooms;
         bathrooms = setBathrooms;
         location = setLocation;
